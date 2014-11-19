9c11daee853f0208811f18360387112d783390b2
Stride <x-stride@users.noreply.github.com>
2014-11-19 16:22:09 +0100

    Typos 	modified:   chapter_5/5_25_Gzip-1.6.sh

--- chapter_5/5_25_Gzip-1.6.sh
+++ chapter_5/5_25_Gzip-1.6.sh
@@ -4,7 +4,7 @@ set -e
 [ ! -d "/tmp/build" ] && mkdir -v /tmp/build
 cd /tmp/build
 
-tar -xvf /mnt/lfs/sources/gzip-1.6
+tar -xvf /mnt/lfs/sources/gzip-1.6.tar.xz
 cd /tmp/build/gzip-1.6
 
 ./configure --prefix=/tools
